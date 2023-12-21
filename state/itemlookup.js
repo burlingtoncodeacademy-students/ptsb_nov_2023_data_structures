4698768baa8be9484e66b5dbd28bfbdd73dd9dad
u4ik <insighteuphoric@gmail.com>
2023-12-21 18:50:11 -0500

    templated inventory obj

--- state/itemlookup.js
+++ state/itemlookup.js
@@ -8,6 +8,40 @@ const rl = readline.createInterface({
 const ask = (questionText) =>
   new Promise((resolve, reject) =>
     rl.question(questionText + "\n", (input) =>
-      input.length > 0 ? resolve(input) : reject("Provide input")
+      input.length > 0 ? resolve(input.toLowerCase()) : reject("Provide input")
     )
   );
+
+let validInvCommands = ["i", "inventory", "items", "inv"];
+
+let inventory = {
+  sword: {
+    name: "Excalibur",
+    description: "A sword pulled from stone",
+    look() {
+      console.log(`${this.name} ⚔️:`);
+      return this.description;
+    },
+  },
+  axe: {
+    name: "Mighty Battle Axe Of Doom!",
+    description: "Great Battle Axe of Doom!",
+    look() {
+      // Show other item info... origin... obtained
+      console.log(`${this.name} 🪓:`);
+      return this.description;
+    },
+    swing() {
+      // Some other FUNCTIONality -> player attacks a target (would prob pass in a target as a param/arg)
+      return `You swing the ${this.name}`;
+    },
+  },
+  hp_potion: {
+    name: "Hp Potion",
+    description: "Potion to restore hp",
+    drink() {
+      // Some other FUNCTIONality -> player restores hp
+      return `You drink the ${this.name}`;
+    },
+  },
+};
